8bf29a4bd2ea64432f3428767aeace252acc0316
nahom <nahom lemi>
2022-10-22 22:12:53 +0300

    unfinished version of mastermind ruby game

--- app.rb
+++ app.rb
@@ -1,36 +1,57 @@
 class Board
+    @@code = ''
     @@correct_order = 0
     @@correct_guss = 0
-    "blue"
-    @@b1 = "|    |    |    |    |"
+
+    @@b1 = "|   |   |   |   |"
     def print_board()
         puts @@b1
         puts "correct order: #{@@correct_order} | correct possition: #{@@correct_guss}"
     end
 end
 
-class Player
+class Player < Board
     @@player_choise = ''
     def player_input()
         puts "insert a 4 digit number from 1-9"
         @@player_choise = gets
-        while player_choise.length != 4;
+        while @@player_choise.length != 4;
             puts 'invalid input insert 4 digits from 1-9:'
             @@player_choise = gets 
+            @@player_choise.to_s
         end
     end
-end
+    def update_board()
+        @@array = @@player_choise.split("")
 
-class Game< Player
-    def start_game()
+        @@b1[2] = @@array[0]
+        @@b1[6] = @@array[1]
+        @@b1[10] = @@array[2]
+        @@b1[14] = @@array[3]
 
+        print_board()
     end
 end
 
-player = Board.new()
-player.print_board()
-player.player_input()
+module Computer
+    def generate_code()
+        @@code = ((rand*10000).floor).to_s
+    end
+end
+class Game< Player
+    include Computer
+    def start_game()
+        print_board()
+        generate_code()
+        player_input()
+        update_board() 
+    end
+end
+
+player = Game.new()
+player.start_game()
 
 #board create and change board anounce current result
 #player get and add input to the board
 #computer generate a n ai and announce the winner
+#game coordinate the game and announce the winner
